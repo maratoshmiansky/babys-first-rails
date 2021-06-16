e7f8188854588bb7edd4a816070556f091a87b58
Marat Oshmiansky <maratical@gmail.com>
2021-06-15 22:37:16 -0700

    inital commit

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,9 @@
+class WelcomesController < ApplicationController
+  def hello_method
+    render json: { message: "Hello from welcomes controller!" }
+  end
+
+  def about_method
+    render json: { message: "Ruby frickin' rocks, dude!" }
+  end
+end
